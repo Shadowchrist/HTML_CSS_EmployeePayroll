9b2f80ee6957e02c287a962b2048e95be9276995
Shadowchrist <sbhm.prasad@gmail.com>
2020-11-16 17:04:05 +0530

    Added UC9-UC10: populating employee object with data entered

--- javascript/payrollData.js
+++ javascript/payrollData.js
@@ -16,3 +16,34 @@ const save = () => {
         return;
     }
 }
+
+const createEmployee = () => {
+    let employeeData = new Employee();
+    employeeData.name = getInputValueById('#name');
+    employeeData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeeData.gender = getSelectedValues('[name=gender]').pop();
+    employeeData.department = getSelectedValues('[name=department]');
+    employeeData.salary = getInputValueById('#salary');
+    employeeData.note = getInputValueById('#notes');
+    let year = getInputValueById('#year');
+    let month = parseInt(getInputValueById('#month'))-1;
+    let day = getInputValueById('#day');
+    employeeData.startDate = new Date(year,month,day);
+    alert(employeeData.toString());
+    return employeeData;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allEmployees = document.querySelectorAll(propertyValue);
+    let selectEmployees = [];
+    allEmployees.forEach(employee => {
+        if(employee.checked)
+        selectEmployees.push(employee.value);
+    });
+    return selectEmployees;
+}
