e3faeba1334036be2da537525f37b82903c3176b
Shadowchrist <sbhm.prasad@gmail.com>
2020-11-16 15:59:07 +0530

    Added UC7-UC8: setting appropriate value for salary

---
+++ javascript/payrollData.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input',function () {
+        output.textContent = salary.value; 
+    });
+    
+});
+
+const save = () => {
+    let employeeData;
+    try{
+        employeeData = createEmployee();
+    }catch(e){
+        return;
+    }
+}
